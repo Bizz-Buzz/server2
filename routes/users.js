9dfdd71b6013debfedeb890fccaa095597e225a9
mischa-saunders <mischa.saunders@gmail.com>
2017-02-01 22:17:49 +1300

    login route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+var bcrypt = require('bcrypt')
+var passport = require('../passport')
+const userDb = require('../db/userDb')
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.json({users: [{name: 'Timmy'}]});
 });
+
+router.post('/login', passport.authenticate('local'), (req, res) => {
+  res.json(req.user)
+})
 
 module.exports = router;
